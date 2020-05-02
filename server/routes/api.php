f4af438017b35e5d245b8ee136ccf49de9fbdfc6
rakib06 <six.rakib@gmail.com>
2020-05-02 06:31:05 +0600

    initial commit

---
+++ server/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('tasks', 'TaskController@index');
+Route::get('tasks/{id}', 'TaskController@show');
+Route::post('tasks', 'TaskController@store');
+Route::put('tasks/{id}', 'TaskController@update');
+Route::delete('tasks/{id}', 'TaskController@delete');
